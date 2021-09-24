88a3b0fcefa1378fdf7c024902a6d6378bbe12b8
MengerZhang <87756644+QuterSilience@users.noreply.github.com>
2021-09-24 17:34:05 +0800

    实现基本功能
    实现基本功能

--- router/router.js
+++ router/router.js
@@ -1,10 +1,14 @@
 // tsc ./router/router.ts --outFile ./router/router.js
 function $(element) {
     if (typeof element === 'string') {
-        var selected = document.querySelectorAll(element);
-        if (!selected) {
-            console.log('Cannot find element: ' + element);
-            return document.createElement('div');
+        var selected;
+        try {
+            selected = document.querySelectorAll(element);
+        }
+        catch (error) {
+            if (!selected) {
+                return null;
+            }
         }
         if (selected.length == 1) {
             return selected[0];
@@ -17,29 +21,88 @@ function $(element) {
 }
 var Router = /** @class */ (function () {
     function Router(option) {
-        var _this = this;
-        console.log(window.location.href);
         this.router = new Map();
-        this.init();
+        this.init(option.complete ? option.complete : null);
         this.hiddenAll();
+        this.reload();
+        this.bindClick();
+    }
+    Router.prototype.init = function (callback) {
+        var _this = this;
+        this.callback = callback;
+        this.activeClass = new Map();
+        window.addEventListener('load', this.reload.bind(this), false);
+        window.addEventListener('hashchange', this.reload.bind(this), false);
+        $('Router').forEach(function (element) {
+            var tag = element.getAttribute('switch');
+            if (element.getAttribute('activeClass')) {
+                _this.activeClass.set(tag, element.getAttribute('activeClass'));
+            }
+            _this.router.set(tag, $("Route[switch=" + tag + "]"));
+        });
+    };
+    Router.prototype.reload = function () {
+        var location = window.location.hash.slice(2, window.location.hash.length);
+        this.selected = location;
+        if ($("Router[switch=" + this.selected + "]").getAttribute('active')) {
+            eval($("Router[switch=" + this.selected + "]").getAttribute('active'));
+        }
+        if (this.callback) {
+            this.callback && this.callback();
+        }
+        this.show(location);
+    };
+    Router.prototype.show = function (tag) {
+        this.hiddenAll();
+        if (this.router.get(tag)) {
+            this.router.get(tag).style.display = 'block';
+        }
+        else {
+            if ($('Route[status=notfound]')) {
+                $('Route[status=notfound]').style.display = 'block';
+            }
+            else {
+                console.warn('do u need [status=notfound] router?');
+            }
+        }
+    };
+    Router.prototype.bindClick = function () {
+        var _this = this;
         this.router.forEach(function (value, key) {
             $("Router[switch=" + key + "]").onclick = function () {
                 _this.hiddenAll();
                 _this.router.get(key).style.display = 'block';
+                _this.selected = key;
+                window.location.hash = '#/' + key;
+                console.log($("Router[switch=" + key + "]"));
+                if ($("Router[switch=" + key + "]").getAttribute('activeClass')) {
+                    console.log($("Router[switch=" + key + "]").classList);
+                    if ($("Router[switch=" + key + "]").classList.length == 0) {
+                        $("Router[switch=" + key + "]").setAttribute('class', $("Router[switch=" + key + "]").getAttribute('activeClass'));
+                    }
+                    else {
+                        $("Router[switch=" + key + "]").classList.add($("Router[switch=" + key + "]").getAttribute('activeClass'));
+                    }
+                }
             };
-        });
-    }
-    Router.prototype.init = function () {
-        var _this = this;
-        $('Router').forEach(function (element) {
-            var tag = element.getAttribute('switch');
-            _this.router.set(tag, $("Route[switch=" + tag + "]"));
         });
     };
     Router.prototype.hiddenAll = function () {
-        $('Switch > Route').forEach(function (element) {
-            element.style.display = 'none';
+        var _this = this;
+        this.router.forEach(function (value, key) {
+            $("Router[switch=" + key + "]").classList.remove(_this.activeClass.get(key));
+            _this.router.get(key).style.display = 'none';
         });
+        if ($('Switch > Route[status]')) {
+            if ($('Switch > Route[status]').length > 1) {
+                $('Switch > Route[status]').forEach(function (element) {
+                    element.style.display = 'none';
+                });
+            }
+            else {
+                $('Switch > Route[status]').style.display = 'none';
+            }
+        }
     };
     return Router;
 }());
--- router/router.ts
+++ router/router.ts
@@ -1,11 +1,15 @@
 // tsc ./router/router.ts --outFile ./router/router.js
 function $(element) {
     if (typeof element === 'string') {
-        var selected = document.querySelectorAll(element);
-        if (!selected) {
-            console.log('Cannot find element: ' + element);
-            return document.createElement('div');
+        var selected;
+        try {
+            selected = document.querySelectorAll(element);
+        } catch (error) {
+            if (!selected) {
+                return null;
+            }
         }
+        
         if (selected.length == 1) {
             return selected[0];
         }
@@ -17,30 +21,97 @@ function $(element) {
 class Router {
     selected: string
     router: Map<string, HTMLDivElement>
+    activeClass: Map<string, string>
+    callback: Function
     constructor(option) {
-        console.log(window.location.href);
         this.router = new Map<string, HTMLDivElement>();
         
-        this.init();
+        this.init(option.complete ? option.complete : null);
 
         this.hiddenAll();
 
-        this.router.forEach((value, key) => {
-            $(`Router[switch=${key}]`).onclick = () => {
-                this.hiddenAll();
-                this.router.get(key).style.display = 'block';
-            }
-        })
+        this.reload();
+
+        this.bindClick();
     }
-    init() {
+    init(callback) {
+        this.callback = callback;
+        this.activeClass = new Map<string, string>();
+        window.addEventListener('load', this.reload.bind(this), false)
+        window.addEventListener('hashchange', this.reload.bind(this), false)
         $('Router').forEach(element => {
             var tag:string = element.getAttribute('switch');
+            if (element.getAttribute('activeClass')) {
+                this.activeClass.set(tag, element.getAttribute('activeClass'));
+            }
             this.router.set(tag, $(`Route[switch=${tag}]`));
         });
     }
+    reload() {
+        var location = window.location.hash.slice(2, window.location.hash.length);
+
+        this.selected = location;
+
+        if ($(`Router[switch=${this.selected}]`).getAttribute('active')) {
+            eval($(`Router[switch=${this.selected}]`).getAttribute('active'));
+        }
+
+        if (this.callback) {
+            this.callback && this.callback();
+        }
+
+        this.show(location);
+    }
+    show(tag) {
+        this.hiddenAll();
+        if (this.router.get(tag)) {
+            this.router.get(tag).style.display = 'block';
+        } else {
+            if ($('Route[status=notfound]')) {
+                $('Route[status=notfound]').style.display = 'block';
+            } else {
+                console.warn('do u need [status=notfound] router?');
+            }
+        }
+    }
+    bindClick() {
+        this.router.forEach((value, key) => {
+            $(`Router[switch=${key}]`).onclick = () => {
+                this.hiddenAll();
+                
+                this.router.get(key).style.display = 'block';
+
+                this.selected = key;
+
+                window.location.hash = '#/' + key;
+
+                console.log($(`Router[switch=${key}]`))
+                if ($(`Router[switch=${key}]`).getAttribute('activeClass')) {
+                    console.log($(`Router[switch=${key}]`).classList);
+                    if ($(`Router[switch=${key}]`).classList.length == 0) {
+                        $(`Router[switch=${key}]`).setAttribute('class', $(`Router[switch=${key}]`).getAttribute('activeClass'));
+                    } else {
+                        $(`Router[switch=${key}]`).classList.add($(`Router[switch=${key}]`).getAttribute('activeClass'));
+                    }
+                }
+                
+            }
+        })
+    }
     hiddenAll() {
-        $('Switch > Route').forEach(element => {
-            element.style.display = 'none';
-        });
+        this.router.forEach((value, key) => {
+            $(`Router[switch=${key}]`).classList.remove(this.activeClass.get(key))
+            this.router.get(key).style.display = 'none';
+        })
+        if ($('Switch > Route[status]')) {
+            if ($('Switch > Route[status]').length > 1) {
+                $('Switch > Route[status]').forEach(element => {
+                    element.style.display = 'none';
+                });
+            } else {
+                $('Switch > Route[status]').style.display = 'none';
+            }
+        }
+        
     }
 }
